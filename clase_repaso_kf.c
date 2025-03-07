2eff58c209fee51a640506c4af264bcd2c9445cd
kfloresj <karla.flores7844@alumnos.udg.mx>
2025-03-07 09:58:57 -0600

    Add files via upload

---
+++ clase_repaso_kf.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int suma (int a, int b){
+		return a + b;
+	}
+
+int main() {
+	
+	int x = 10, y = 20, resultado = 0;
+	
+	printf("La suma es %d ");
+	
+	
+	/*
+	int A[5];
+	printf("%d", A),
+	printf("%d",&A);
+	printf("%d",A[0]);
+	
+	
+	int numeros[5] = {10, 20, 30, 40, 50};
+	int i=0;
+	for (i=0;i<5;i++){
+		printf("Elemento %d: %d \n",i, numeros[i]);
+	}
+	
+	
+	int matriz[2][3] = {{1, 2, 3}, {4, 5, 6}};
+	int i,j;
+	for (i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			printf("Elemento [%d][%d]: %d \n",i,j,matriz[i][j]);
+		}
+	}
+	
+	*/
+	
+	
+	return 0;
+}
